0e95cebd0433e43e4e1bbd860764342a911776a5
khan <khan.muslehuddin0001@gmail.com>
2023-06-11 18:07:02 +0600

    # Feature :sparkles: - Lesson on Mod

---
+++ hitesh/23Module/go.mod
@@ -0,0 +1,5 @@
+module github.com/musleh0001/go-basic
+
+go 1.20
+
+require github.com/gorilla/mux v1.8.0
---
+++ hitesh/23Module/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Lesson on Module")
+	greeter()
+	r := mux.NewRouter()
+	r.HandleFunc("/", serveHome).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":4000", r))
+}
+
+func greeter() {
+	fmt.Println("Hey there mod users")
+}
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Welcome to goland</h1>"))
+}
